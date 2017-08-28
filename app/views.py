0714247d02d699e12e048224b1e03ad24911bff6
Shashank Gupta <shashankgupta1994@gmail.com>
2017-08-28 17:56:21 +0530

    further views

--- app/views.py
+++ app/views.py
@@ -4,10 +4,10 @@ from flaskext.mysql import MySQL
 import requests
 import os
 
-MYSQL_DATABASE_HOST	= 'us-cdbr-iron-east-05.cleardb.net'
-MYSQL_DATABASE_USER	= 'b644ea1641c355'
-MYSQL_DATABASE_PASSWORD	= '6883ba6d'
-MYSQL_DATABASE_DB = 'heroku_ce775df7aab92ed'
+# MYSQL_DATABASE_HOST	= 'us-cdbr-iron-east-05.cleardb.net'
+# MYSQL_DATABASE_USER	= 'b644ea1641c355'
+# MYSQL_DATABASE_PASSWORD	= '6883ba6d'
+# MYSQL_DATABASE_DB = 'heroku_ce775df7aab92ed'
 
 app = Flask(__name__)
 mysql = MySQL()
