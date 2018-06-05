495ea634ac10f2c3a037f7335df63f372e8780be
Shashank Gupta <shashankgupta1994@gmail.com>
2018-06-06 03:09:21 +0530

    heading style

--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 # app.config['MYSQL_DATABASE_USER'] = 'root'
 # app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-# app.config['MYSaQL_DATABASE_DB'] = 'pragyapravah'
+# app.config['MYSQL_DATABASE_DB'] = 'pragyapravah'
 
 app.config['MYSQL_DATABASE_HOST'] = 'sql12.freesqldatabase.com'
 app.config['MYSQL_DATABASE_USER'] = 'sql12198115'
